3f0906f34669998d325e881ebe119bbb90aa9f7b
Salamatov <dimasalam@yandex.ru>
2020-02-05 09:01:33 +0300

    module1-task2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
+
+
+const App = () => {
+  return (
+    <WelcomeScreen/>
+  );
+};
+
+export default App;
