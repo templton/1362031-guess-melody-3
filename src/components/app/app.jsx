dc4f71a3b6c5c332b73eb057f5c7a47dff43221c
Salamatov <dimasalam@yandex.ru>
2020-02-05 09:10:04 +0300

    module1-task2

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import WelcomeScreen from "../welcome-screen/welcome-screen";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+
   return (
-    <WelcomeScreen/>
+    <WelcomeScreen errorsCount={errorsCount}/>
   );
 };
 
