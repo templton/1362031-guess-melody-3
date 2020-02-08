88d9997a209b3a0dd5164eed7d255f9e36d7d61c
Salamatov <dimasalam@yandex.ru>
2020-02-08 18:26:41 +0300

    master enzyme configure

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -1,11 +1,6 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import {shallow} from "enzyme";
 import WelcomeScreen from "./welcome-screen";
-
-Enzyme.configure({
-  adapter: new Adapter(),
-});
 
 it(`Should welcome button be pressed`, () => {
   const onWelcomeButtonClick = jest.fn();
