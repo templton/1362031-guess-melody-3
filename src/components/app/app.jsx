eb602a511df9d5ae8bbf93f759318113c182d9d1
Salamatov <dimasalam@yandex.ru>
2020-02-11 09:39:29 +0300

    3-10

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter,Route,Switch} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestion from "../artist-question/artist-question.jsx";
+import GenreQuestion from "../genre-question/genre-question.jsx";
 
 const welcomeButtonHandler = () => {};
 
@@ -9,15 +12,28 @@ const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorsCount={errorsCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestion/>
+        </Route>
+        <Route axact path="/dev-genre">
+          <GenreQuestion/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired
 };
 
 export default App;
