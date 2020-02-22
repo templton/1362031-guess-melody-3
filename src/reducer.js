041d66c6c946db4c9578267d09c16288f4f44cde
Salamatov <dimasalam@yandex.ru>
2020-02-22 15:06:34 +0300

    module5-task1 подключил redux + тесты для reducer

---
+++ src/reducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  mistakes: 0,
+  step: -1
+};
+
+const ActionType = {
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
+  INCREMENT_STEP: `INCREMENT_STEP`
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_STEP:
+      return Object.assign({}, state, {step: state.step + 1});
+    case ActionType.INCREMENT_MISTAKES:
+      return Object.assign({}, state, {mistakes: state.mistakes + 1});
+  }
+
+  return state;
+};
+
+export {reducer, ActionType};
---
+++ src/reducer.test.js
@@ -0,0 +1,18 @@
+import {reducer, ActionType} from "./reducer";
+
+describe(`Test main reducer`, () => {
+
+  const state = {step: -1, mistakes: 0};
+
+  it(`Empty action return initial state`, () => {
+    expect(reducer(void 0, {})).toEqual(state);
+  });
+
+  it(`Increment mistakes`, () => {
+    expect(reducer(state, {type: ActionType.INCREMENT_MISTAKES})).toEqual({step: -1, mistakes: 1});
+  });
+
+  it(`Increment step`, () => {
+    expect(reducer(state, {type: ActionType.INCREMENT_STEP})).toEqual({step: 0, mistakes: 0});
+  });
+});
